c0b302a5b6164f01e7546f317f8844f64787483b
Ridhima Khandelwal <208533492+ridhima0511@users.noreply.github.com>
2026-06-08 18:22:07 +0530

    Create ContainerWithMostWater.cpp

---
+++ Week_1/Day_3/ContainerWithMostWater.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int ans = 0;
+        while(left < right) {
+            int width = right - left;
+            int currArea =
+                min(height[left], height[right]) * width;
+            ans = max(ans, currArea);
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return ans;
+    }
+};
